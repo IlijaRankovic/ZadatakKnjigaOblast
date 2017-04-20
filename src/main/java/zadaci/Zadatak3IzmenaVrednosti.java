91d52ddd4d39065c7f5846c33dbca95ec89f27f4
Ilija <ilija@gmail.com>
2017-04-20 19:21:30 +0200

    Kreirane klase za zadatke fixes #3

---
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -0,0 +1,31 @@
+package zadaci;
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by androiddevelopment on 20.4.17..
+ */
+public class Zadatak3IzmenaVrednosti {
+
+    public static void main(String[] args) {
+        ConnectionSource conn = null;
+
+        try {
+            conn = new JdbcConnectionSource("jdb:sqlite:knjigaOblast.db");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            if (conn != null){
+                try {
+                    conn.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
