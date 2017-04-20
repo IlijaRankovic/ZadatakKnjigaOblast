5ce381b65485a829ed07d5a3475e20139bb8714b
Ilija <ilija@gmail.com>
2017-04-20 20:31:29 +0200

    Uradjen zadatak kreiranja tabela fixes #7

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -2,6 +2,9 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Knjiga;
+import model.Oblast;
 
 import java.io.IOException;
 import java.sql.SQLException;
@@ -16,6 +19,11 @@ public class Zadatak1KreiranjeTabela {
 
         try {
             conn = new JdbcConnectionSource("jdb:sqlite:knjigaOblast.db");
+            TableUtils.dropTable(conn, Oblast.class, true);
+            TableUtils.dropTable(conn, Knjiga.class, true);
+
+            TableUtils.clearTable(conn, Knjiga.class);
+            TableUtils.clearTable(conn, Oblast.class);
         } catch (SQLException e) {
             e.printStackTrace();
         }finally {
